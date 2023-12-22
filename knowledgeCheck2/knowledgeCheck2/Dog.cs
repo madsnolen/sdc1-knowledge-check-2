8cd2ab71516ebf133f3e258a96dbfd6da555a339
Madison <madisonnolen03@gmail.com>
2023-12-22 11:18:17 -0800

    I did it!

---
+++ knowledgeCheck2/knowledgeCheck2/Dog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace knowledgeCheck2
+{
+    public class Dog : DogType
+    {
+        public string? DogBreed { get; set; }
+    }
+}
---
+++ knowledgeCheck2/knowledgeCheck2/Program.cs
@@ -0,0 +1,38 @@
+﻿using knowledgeCheck2;
+
+Console.WriteLine("How many dogs do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<Dog>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    var dog = new Dog();
+
+    Console.WriteLine("Enter the name of your dog: ");
+    dog.Name = Console.ReadLine();
+
+    Console.Write("How Old is this dog? Enter Age: ");
+    dog.Age = int.Parse(Console.ReadLine());
+
+    Console.Write("Enter the type of Breed: ");
+    dog.DogBreed = Console.ReadLine();
+
+
+    recordList.Add(dog);
+}
+
+// Print out the list of records using Console.WriteLine()
+Console.WriteLine("*****************************");
+Console.WriteLine($"Name:      {Dog.Name}");
+Console.WriteLine($"Age:       {Dog.Age}");
+Console.WriteLine($"Dog Type: {Dog.DogBreed}");
+Console.WriteLine();
+foreach (var Dog in recordList)
+{
+    Console.WriteLine("*****************************");
+    Console.WriteLine($"Name:      {Dog.Name}");
+    Console.WriteLine($"Age:       {Dog.Age}");
+    Console.WriteLine($"Dog Type: {Dog.DogBreed}");
+    Console.WriteLine();
+}
